5ad039ae0a5c1023aea4a796f623d1cb8f3e8516
jack <jack@ubuntu2.myguest.virtualbox.org>
2026-05-17 05:31:28 +0800

    fix django allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'monitoring.middleware.ActivityLogMiddleware',   # Last – capture authenticated user
+    #'monitoring.middleware.ActivityLogMiddleware',   # Last – capture authenticated user
     'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
