0f3266dcecc31756331a728e69c7cba150d4762d
jack <jack@ubuntu2.myguest.virtualbox.org>
2026-05-16 20:44:07 +0800

    Increase migrate job memory

--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -9,7 +9,7 @@ variable "namespace" { type = string } # K8s namespace the runtime SA binds into
 variable "kubernetes_service_accounts" {
   type        = list(string)
   description = "K8s ServiceAccount names that can impersonate the runtime GSA."
-  default     = ["lims-lims"] # default chart fullname (lims release × lims chart)
+  default     = ["lims-prod-lims"] # default chart fullname (lims release × lims chart)
 }
 
 variable "enable_ci_builder" {
